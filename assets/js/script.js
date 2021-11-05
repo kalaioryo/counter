c99fc9ad68fa166e727998941d8cc972e75b90c0
kalaioryo <kalaioryo@gmail.com>
2021-11-05 12:04:07 +0100

    Implementato html tramite javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,103 @@
+//structure
+
+//main div
+const mainDiv = document.getElementById('main');
+
+
+//display
+const display = document.createElement('div');
+mainDiv.appendChild(display);
+display.classList.add('display');
+
+const textDisplay = document.createElement('h3');
+display.appendChild(textDisplay);
+textDisplay.innerHTML = '0';
+
+
+//container button
+const containerButton = document.createElement('div');
+mainDiv.appendChild(containerButton);
+containerButton.classList.add('container-button');
+
+
+//minus button
+const minusButton = document.createElement('div');
+const minusImg = document.createElement('img');
+
+containerButton.appendChild(minusButton);
+minusButton.appendChild(minusImg);
+
+minusButton.setAttribute('id', 'button-minus');
+minusButton.classList.add('button-minus', 'pointer');
+
+minusImg.setAttribute('src', "assets/img/minus.svg");
+minusImg.setAttribute('alt', 'minus');
+
+
+//plus button
+const plusButton = document.createElement('div');
+const plusImg = document.createElement('img');
+
+containerButton.appendChild(plusButton);
+plusButton.appendChild(plusImg);
+
+plusButton.setAttribute('id', 'button-plus');
+plusButton.classList.add('button-plus', 'pointer');
+
+plusImg.setAttribute('src', "assets/img/plus.svg");
+plusImg.setAttribute('alt', 'plus');
+
+
+//reset button
+const resetButton = document.createElement('div');
+const resetText = document.createElement('h2');
+
+mainDiv.appendChild(resetButton);
+resetButton.appendChild(resetText);
+resetText.innerHTML = 'reset';
+
+
+resetButton.setAttribute('id', 'reset');
+resetButton.classList.add('reset', 'pointer');
+
+
+//variables and eventListener
+
 let minus = document.getElementById('button-minus');
 let plus = document.getElementById('button-plus');
 let reset = document.getElementById('reset');
-let number = document.getElementById('number');
-let h3 = document.querySelector('h3');
 
-number = 0;
 
 plus.addEventListener('click', add);
 minus.addEventListener('click', Subtract);
 reset.addEventListener('click', zero);
 
 
-
+// function
 
 function add(){
-    number++;
-    h3.textContent = number;
+    textDisplay.innerHTML++;
 }
 
 function Subtract(){
-    number--;
-    h3.textContent = number;
+    textDisplay.innerHTML--;
 }
 
 function zero(){
-    number = 0;
-    h3.textContent = number;
+    textDisplay.innerHTML = 0;
 }
+
+//keyboard function
 
 document.addEventListener('keydown', function(event) {
     if (event.code == "NumpadAdd" ) {
-        number++;
-        h3.textContent = number;
+        textDisplay.innerHTML++;
+
     } else if (event.code == "NumpadSubtract" ){
-        number--;
-        h3.textContent = number;
+        textDisplay.innerHTML--;
+
     } else if (event.code == 'Delete') {
-        number = 0;
-        h3.textContent = number;
+        textDisplay.innerHTML = 0;
+
     } else return
   });
